14e4500269bce96b850dd6db34c109f16121ce49
Eugene <e.tetin.another@gmail.com>
2019-11-16 17:38:24 +0300

    Updated server and client classes. Added decorator for GET requests. Has been structured code and methods for handling client request. Now we can store request info and process request of index page, also added handler for 404 status.

--- client.py
+++ client.py
@@ -1,10 +1,45 @@
-class Client:
-    def __init__(self, socket):
-        self.socket = socket
+from copy import copy
 
-    def handle(self):
-        request = self.socket.recv(4096)
-        if request:
-            self.socket.send(b'hello\n')
+
+class Request:
+    pass
+
+
+class Client:
+    def __init__(self, socket, handlers):
+        self.socket = socket
+        self.handlers = handlers
+
+    def handle_request(self, request):
+        for handler in self.handlers:
+            if handler.can_handle(request=request):
+                return handler.handle(request)
         else:
-            self.socket.close()
+            return None  # no handlers
+
+    def parse_request(self):
+        raw_request = self.socket.recv(4096).decode('utf-8').splitlines()
+
+        request = Request()
+        request_info = raw_request.pop(0)
+        request.method, request.path, request.http_version = request_info.split()  # METHOD /path HTTP/version
+        request.data = raw_request.pop()
+
+        # TODO parse headers
+        request.headers = copy(raw_request)
+
+        return request
+
+    def send_response(self, response):
+        code, body = response
+        body = body.encode()
+
+        code_name = {
+            200: 'OK',
+            404: 'Not Found',
+        }[code]
+
+        self.socket.send(f'HTTP/1.0 {code} {code_name}\r\n'.encode())
+
+        self.socket.send(body)
+
--- server.py
+++ server.py
@@ -8,21 +8,63 @@ class Server:
         self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((host, port))
+        self.handlers = []
 
     def run(self):
         self.socket.listen()
         while True:
             client_socket, client_addr = self.socket.accept()
-            print('client here')
-
             self.handle_client(client_socket=client_socket)
+
             client_socket.close()
 
-    @staticmethod
-    def handle_client(client_socket):
-        client = Client(client_socket)
-        client.handle()
+    def handle_client(self, client_socket):
+        client = Client(client_socket, self.handlers)
+        request = client.parse_request()
+        response = client.handle_request(request=request)
+        if response is None:
+            # TODO return 404 or 405
+            response = 404, '''
+                <html>
+                    <head>
+                        <title>page not found</title>
+                    </head>
+                    <body>
+                        <h1>404 not found</h1>
+                    </body>
+                </html>'''
+
+        client.send_response(response=response)
+
+    def get(self, path):
+        def decorator(f):
+            class Handler:
+                def can_handle(self, request):
+                    return request.method == 'GET' and request.path == path
+
+                def handle(self, request):
+                    return f(request)
+
+            self.handlers.append(Handler())
+            return f
+
+        return decorator
 
 
 server = Server()
+
+
+@server.get('/')
+def root(request):
+    return 200, '''
+        <html>
+            <head>
+                <title>index page</title>
+            </head>
+            <body>
+                <h1>hello</h1>
+            </body>
+        </html>'''
+
+
 server.run()
